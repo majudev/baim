eca28e407a43ace8e0d842bc5d259c06455cd8f4
Michał Maj <maju@majudev.net>
2023-12-17 18:05:34 +0100

    Add rate limiting

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import requestLogger from './utils/requestLogger';
 import basicAuth from 'express-basic-auth';
+import { rateLimit } from 'express-rate-limit'
 
 const app = express();
 
@@ -13,7 +14,18 @@ app.use('/auth', authRouter);
 
 app.use(basicAuth({
     users: { 'admin': 'supersecret' }
-}))
+}));
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Use an external store for consistency across multiple server instances.
+});
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
 
 const port = 9030;
 app.listen(port, () => {
